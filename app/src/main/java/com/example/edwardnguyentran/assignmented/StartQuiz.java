5e4b0d1a0d5b1e2e6e7f1eab18cb4ebe96af40e7
Edward Nguyen Tran <edwardnguyentran@Edwards-MacBook-Pro.local>
2018-10-24 05:16:43 +1100

    Initial commit

---
+++ app/src/main/java/com/example/edwardnguyentran/assignmented/StartQuiz.java
@@ -0,0 +1,70 @@
+package com.example.edwardnguyentran.assignmented;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StartQuiz extends Fragment {
+
+
+    private static final int REQUEST_CODE_QUIZ = 1;
+
+    public StartQuiz() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View v =inflater.inflate(R.layout.fragment_start_quiz, container, false );
+
+
+        Button buttonStartQuiz =v.findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction fr=getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment,new Quiz());
+                fr.commit();
+
+            }
+        });
+
+        return v;
+    }
+
+    }
+
+//        private void startQuiz() {
+//            Intent intent = new Intent(getActivity(), Quiz.class);
+//            startActivityForResult(intent, REQUEST_CODE_QUIZ);
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
